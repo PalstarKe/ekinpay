427c2c0155fc22e84832abb0eb9d325dbf681813
root <root@vmi2393658.contaboserver.net>
2025-03-12 15:23:29 +0300

    Added FUP and Tr069

--- app/Http/Controllers/FupController.php
+++ app/Http/Controllers/FupController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class FupController extends Controller
 {
-    //
+    public function index()
+    {
+        if (Auth::user()->can('manage fup')) {
+            // $packages = Package::with('bandwidth')->where('created_by', Auth::user()->creatorId())->get();
+            // $admin_payment_setting = Utility::getCompanyPaymentSetting(Auth::user()->creatorId());
+            // foreach ($packages as $package) {
+            //     $package->status = $this->isPackagesActive($package) ? 'Active' : 'Inactive';
+            // }
+            // // return view('package.index', compact('packages'));
+            // $pppoePackages = $packages->where('type', 'PPPoE');
+            // $hotspotPackages = $packages->where('type', 'Hotspot');
+            return view('fup.index', compact());
+        } else {
+            return redirect()->back()->with('error', __('Permission denied.'));
+        }
+    }
 }
--- app/Http/Controllers/Tr069Controller.php
+++ app/Http/Controllers/Tr069Controller.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class Tr069Controller extends Controller
 {
-    //
+    public function index()
+    {
+        if (Auth::user()->can('manage tr069')) {
+            // $packages = Package::with('bandwidth')->where('created_by', Auth::user()->creatorId())->get();
+            // $admin_payment_setting = Utility::getCompanyPaymentSetting(Auth::user()->creatorId());
+            // foreach ($packages as $package) {
+            //     $package->status = $this->isPackagesActive($package) ? 'Active' : 'Inactive';
+            // }
+            // // return view('package.index', compact('packages'));
+            // $pppoePackages = $packages->where('type', 'PPPoE');
+            // $hotspotPackages = $packages->where('type', 'Hotspot');
+            return view('tr069.index', compact());
+        } else {
+            return redirect()->back()->with('error', __('Permission denied.'));
+        }
+    }
+
 }
